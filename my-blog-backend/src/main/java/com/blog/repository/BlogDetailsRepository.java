29f8b2d5669a407c6139c6e1994298bdc3c9b181
JavaCoderSubham <subham.gupta@motivitylabs.com>
2023-06-11 23:06:49 +0530

    Repository change

--- my-blog-backend/src/main/java/com/blog/repository/BlogDetailsRepository.java
+++ my-blog-backend/src/main/java/com/blog/repository/BlogDetailsRepository.java
@@ -3,5 +3,15 @@ package com.blog.repository;
 import com.blog.entity.BlogDetails;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface BlogDetailsRepository extends MongoRepository<BlogDetails, String> {
+
+    List<BlogDetails> findByAuther(String auther);
+    List<BlogDetails> findByTitleStartingWith(String title);
+    List<BlogDetails> findByTopicStartingWith(String topic);
+    List<BlogDetails> findByEmail(String email);
+    List<BlogDetails> findByBlogTextStartingWith(String blogText);
+
+
 }
